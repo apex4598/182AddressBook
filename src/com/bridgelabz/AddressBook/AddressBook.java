d65e1cc665c5b5471f1861feed8e56fe8f2554cf
Apex4598 <login2ajit@gmail.com>
2022-08-13 06:12:48 +0530

    Added : Functionality to Delete Contact in Address Book

--- src/com/bridgelabz/AddressBook/AddressBook.java
+++ src/com/bridgelabz/AddressBook/AddressBook.java
@@ -7,28 +7,32 @@ public class AddressBook {
     Scanner scanner = new Scanner(System.in);
     ArrayList<Contact> list = new ArrayList<>();
 
-    public void operations(ArrayList<Contact> arrayRead){
+    public void operations(ArrayList<Contact> arrayRead) {
         list = arrayRead;
         boolean status = true;
 
-        do{
+        do {
             System.out.println("Choose Operations : ");
-            System.out.println("1. Add\n 2. Edit\n 3. Back");
-            switch (scanner.next()){
+            System.out.println("1. Add\n 2. Edit\n 3. Delete \n 4. Back");
+            switch (scanner.nextInt()) {
                 case 1:
                     add();
                     break;
                 case 2:
                     edit();
                     break;
-
+                case 3:
+                    delete();
+                    break;
+                case 4:
+                    status = false;
+                    break;
             }
-            add();
 
-        }while (status);
+        } while (status);
     }
 
-    public void add(){
+    public void add() {
         Contact contact = new Contact();
         System.out.println("Enter the First name:");
         String firstName = scanner.next();
@@ -61,41 +65,57 @@ public class AddressBook {
         list.add(contact);
 
     }
-    public void edit(){
+
+    public void edit() {
         System.out.println("Enter your First name:");
         String firstname = scanner.next();
 
-       Iterator<Contact> iterator = list.listIterator();
+        Iterator<Contact> iterator = list.listIterator();
 
-       while (iterator.hasNext()){
-           Contact contact = iterator.next();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
 
-           if(firstname.equals(contact.getFirstName(firstname))){
-               System.out.println("Choose field you want to Edit:");
-               System.out
-                       .println("1.Last Name\t2.Phone Number\t3.City\t4.Zip\t5. State");
-               switch (scanner.nextInt()) {
-                   case 1:
-                       System.out.println("Re-Correct your lastname");
-                       contact.setLastName(contact.getLastName());
-                       break;
-                   case 2:
-                       System.out.println("Re-Correct your Phone Number");
-                       contact.getPhoneNumber(scanner.nextLong());
-                       break;
-                   case 3:
-                       System.out.println("Re-Correct your City");
-                       contact.setCity(scanner.next());
-                       break;
-                   case 4:
-                       System.out.println("Re-Correct your Zip");
-                       contact.setZip(scanner.nextLong());
-                       break;
-                   case 5:
-                       System.out.println("Re-Correct your State");
-                       contact.setState(scanner.next());
-                       break;
-           }
-       }
+            if (firstname.equals(contact.getFirstName(firstname))) {
+                System.out.println("Choose field you want to Edit:");
+                System.out
+                        .println("1.Last Name\t2.Phone Number\t3.City\t4.Zip\t5. State");
+                switch (scanner.nextInt()) {
+                    case 1:
+                        System.out.println("Re-Correct your lastname");
+                        contact.setLastName(contact.getLastName());
+                        break;
+                    case 2:
+                        System.out.println("Re-Correct your Phone Number");
+                        contact.getPhoneNumber(scanner.nextLong());
+                        break;
+                    case 3:
+                        System.out.println("Re-Correct your City");
+                        contact.setCity(scanner.next());
+                        break;
+                    case 4:
+                        System.out.println("Re-Correct your Zip");
+                        //contact.setZip(scanner.nextLong());
+                        break;
+                    case 5:
+                        System.out.println("Re-Correct your State");
+                        contact.setState(scanner.next());
+                        break;
+                }
+            }
+        }
+    }
+    public void delete(){
+        System.out.println("Enter First Name ");
+        String firstName = scanner.next();
+
+        Iterator<Contact> iterator = list.listIterator();
+        while ((iterator.hasNext())){
+            Contact contact = iterator.next();
+
+            if(firstName.equals(contact.getFirstName(firstName))){
+                list.remove(contact);
+                
+            }
+        }
     }
 }
